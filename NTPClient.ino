2e3a1e51be0d0af3281a77fff6e1772139018c35
Seyed Mahyar Askarinejad Moghaddam <askari.m1379@gmail.com>
2023-06-03 13:46:52 +0330

    Update NTPClient.ino

--- NTPClient.ino
+++ NTPClient.ino
@@ -1,12 +1,13 @@
 #include "NTPClient.h"
 
 NTPClient ntpClient;
-const char IP[] = "time.nist.gov"; //time.nist.gov  //192.168.2.3
+const char IP[] = "time.nist.gov"; //time.nist.gov
+unsigned int port = 8888;
 String dateTime = "";
 
 void setup() {
   Serial.begin(9600);
-  if(!ntpClient.begin(IP))
+  if(!ntpClient.begin(IP, port))
   {
     Serial.println("Can not begin NTP-Client!");
   }
@@ -16,4 +17,5 @@ void setup() {
 void loop() {
   dateTime = ntpClient.GetDateTime();
   Serial.println(dateTime);
+  delay(10000);
 }
